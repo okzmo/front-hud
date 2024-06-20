82290c160cb237bd937595cdc991b52c71a22ae9
Mathieu Rossi <mathieu-rossi.pro@proton.me>
2024-06-20 12:11:27 +0200

    fix: crash when accessing illegal routes

--- src/routes/hudori/chat/community/[serverId]/+page.svelte
+++ src/routes/hudori/chat/community/[serverId]/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
 	import { server } from '$lib/stores';
 	import type { Server } from '$lib/types';
-	import { redirect } from '@sveltejs/kit';
 
 	export let data: { server: Server };
 	$: server.set(data.server);
 	$: if ($server) {
 		const serverId = $server.id.split(':')[1];
 		const channelId = $server.categories[0].channels[0].id.split(':')[1];
-		goto(`${serverId}/channels/${channelId}`);
+		if (browser) {
+			goto(`${serverId}/channels/${channelId}`);
+		}
 	}
 </script>
