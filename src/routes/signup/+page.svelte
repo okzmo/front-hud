ab8bfda1d6aad39f7369792cb61e29cfe045add7
Mathieu Rossi <mathieu-rossi.pro@proton.me>
2024-06-23 00:38:09 +0200

    feat: remove signup for now

--- src/routes/signup/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import SignupForm from '$lib/components/connection/signup-form.svelte';
-	import type { PageData } from './$types.js';
-
-	export let data: PageData;
-</script>
-
-<SignupForm data={data.form} />
